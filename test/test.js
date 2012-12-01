f3d9acbba3e7eb6ec416fdc318260a4158f6cc59
Andrew Brampton <me@bramp.net>
2012-12-01 10:10:40 -0500

    Changed the Parser to be a event emitter.

--- test/test.js
+++ test/test.js
@@ -1,14 +1,28 @@
 var fs   = require('fs');
 var hessian = require('../lib/hessian.js');
 
+var parser = new hessian.HessianParser();
+
+parser.on('object', function(obj, offset) {
+	console.log (obj);
+});
+
+parser.on('call', function(call, offset) {
+	console.log (call);
+});
+
+parser.on('reply', function(reply, offset) {
+	console.log (reply);
+});
+
+parser.on('error', function(err) {
+	console.log (err);
+});
+
 var testFile = 'testdata/MOs';
 fs.readFile(testFile, function (err, buf) {
 	if (err) throw err;
-	console.log(buf);
 
-	hessian.parse( buf, function(obj, offset) {
-		console.log (obj);
-	} ); /* .on('error', ...) */
-
+	parser.parse(buf);
 
 });
