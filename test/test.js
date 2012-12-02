bb2cc4c5f3fae3889c9d0ef91c8be3416869e867
Andrew Brampton <me@bramp.net>
2012-12-02 17:56:21 -0500

    Multiple changes to allow the decoder to function on unfinished data. This is needed if streaming Hessian over a socket, and it doesn't all come in a single packet.

--- test/test.js
+++ test/test.js
@@ -19,10 +19,14 @@ parser.on('error', function(err) {
 	console.log (err);
 });
 
-var testFile = 'testdata/MOs';
+var testFile = 'testdata/subscribe-server';
 fs.readFile(testFile, function (err, buf) {
 	if (err) throw err;
 
-	parser.decode(buf);
+	// Split up the buffer into lots of small buffers
+	// Terrible performance, but absolutely worst case!
+	for (var i = 0; i < buf.length; i++) {
+		parser.decode(buf.slice(i, i+1));
+	}
 
 });
